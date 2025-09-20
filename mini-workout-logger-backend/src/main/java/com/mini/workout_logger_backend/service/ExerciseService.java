16ca8cd1bcefd8c50829dce33fdde7ee1f4c6ee1
Carolina <naccaratocarolina@gmail.com>
2025-09-20 15:51:27 -0300

    Recursivily find muscle groups

--- mini-workout-logger-backend/src/main/java/com/mini/workout_logger_backend/service/ExerciseService.java
+++ mini-workout-logger-backend/src/main/java/com/mini/workout_logger_backend/service/ExerciseService.java
@@ -3,8 +3,11 @@ package com.mini.workout_logger_backend.service;
 import com.mini.java_core.service.AbstractService;
 import com.mini.workout_logger_backend.dto.ExerciseDTO;
 import com.mini.workout_logger_backend.entity.Exercise;
+import com.mini.workout_logger_backend.entity.Muscle;
 import com.mini.workout_logger_backend.mapper.ExerciseMapper;
 import com.mini.workout_logger_backend.repository.ExerciseRepository;
+import com.mini.workout_logger_backend.repository.MuscleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +15,20 @@ public class ExerciseService extends AbstractService<Exercise,
                                                      ExerciseDTO,
                                                      ExerciseMapper,
                                                      ExerciseRepository> {
+
+    @Autowired
+    MuscleService muscleService;
+
+    @Override
+    public Exercise transform(Exercise entity) {
+        // Adds the muscle groups associated with the muscle.
+        for (Muscle muscle : entity.getMuscles()) {
+            for (Muscle parent : muscleService.findParentMusclesRecursive(muscle, new java.util.HashSet<>())) {
+                entity.addMuscle(parent);
+            }
+        }
+
+        return super.transform(entity);
+    }
 
 }
--- mini-workout-logger-backend/src/main/java/com/mini/workout_logger_backend/service/MuscleService.java
+++ mini-workout-logger-backend/src/main/java/com/mini/workout_logger_backend/service/MuscleService.java
@@ -1,16 +1,46 @@
 package com.mini.workout_logger_backend.service;
 
+import com.mini.java_core.dto.ResponseDTO;
+import com.mini.java_core.entity.ResponseHelper;
+import com.mini.java_core.enums.ResponseMessage;
 import com.mini.java_core.service.AbstractService;
 import com.mini.workout_logger_backend.dto.MuscleDTO;
 import com.mini.workout_logger_backend.entity.Muscle;
 import com.mini.workout_logger_backend.mapper.MuscleMapper;
 import com.mini.workout_logger_backend.repository.MuscleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class MuscleService extends AbstractService<Muscle,
-                                                        MuscleDTO,
-                                                        MuscleMapper,
-                                                        MuscleRepository> {
+                                                   MuscleDTO,
+                                                   MuscleMapper,
+                                                   MuscleRepository> {
+
+    public Set<Muscle> findParentMusclesRecursive(Muscle muscle, Set<Muscle> parents) {
+        if (muscle.getMuscleGroups() != null) {
+            for (Muscle parent : muscle.getMuscleGroups()) {
+                if (parents.add(parent)) {
+                    findParentMusclesRecursive(parent, parents);
+                }
+            }
+        }
+        return parents;
+    }
+
+    public ResponseEntity<ResponseDTO<MuscleDTO>> getParentMuscles(Long muscleId) {
+        Muscle muscle = repository.safeFindById(muscleId);
+        Set<Muscle> parentMuscles = findParentMusclesRecursive(muscle, new java.util.HashSet<>());
+        return ResponseHelper.success(
+                HttpStatus.OK,
+                ResponseMessage.ENTITIES_FOUND.getMessage(),
+                parentMuscles.stream()
+                             .map(mapper::toDTO)
+                             .collect(Collectors.toList()));
+    }
 
 }
