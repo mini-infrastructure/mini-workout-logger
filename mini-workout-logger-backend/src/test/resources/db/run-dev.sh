b3f22e6ce4df835e8530550b04b9632f5889bbb1
Carolina Naccarato <naccaratocarolina@gmail.com>
2025-10-31 17:40:12 -0300

    Update to docker compose command

--- mini-workout-logger-backend/src/test/resources/db/run-dev.sh
+++ mini-workout-logger-backend/src/test/resources/db/run-dev.sh
@@ -6,11 +6,11 @@ POSTGRES_DATA="../../assets/postgres-data"
 
 function up() {
   mkdir -p "$PGADMIN_DATA" "$POSTGRES_DATA"
-  docker-compose -f "$COMPOSE_PATH" up -d
+  docker compose -f "$COMPOSE_PATH" up -d
   sudo chown -R 5050:5050 "$PGADMIN_DATA"
 
   echo "Waiting for PostgreSQL to start..."
-  until docker-compose -f "$COMPOSE_PATH" exec -T db pg_isready -U postgres; do
+  until docker compose -f "$COMPOSE_PATH" exec -T db pg_isready -U postgres; do
     echo "PostgreSQL is not ready yet, waiting..."
     sleep 1
   done
@@ -19,7 +19,7 @@ function up() {
 }
 
 function down() {
-  docker-compose -f "$COMPOSE_PATH" down -v
+  docker compose -f "$COMPOSE_PATH" down -v
   sudo rm -rf "$PGADMIN_DATA" "$POSTGRES_DATA"
   echo "PostgreSQL and pgAdmin containers stopped and data directories removed."
 }
