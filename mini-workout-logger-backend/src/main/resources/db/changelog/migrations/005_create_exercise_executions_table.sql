280ba638201de650a18d84217104a8b15512d24e
Carolina Naccarato <naccaratocarolina@gmail.com>
2025-11-01 19:00:20 -0300

    Create exercise executions table

---
+++ mini-workout-logger-backend/src/main/resources/db/changelog/migrations/005_create_exercise_executions_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE exercise_executions (
+
+    id BIGSERIAL PRIMARY KEY,
+    exercise_id BIGINT NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    equipment VARCHAR(255) NOT NULL,
+    repetitions INTEGER,
+    weight DOUBLE PRECISION,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id) ON UPDATE CASCADE ON DELETE RESTRICT
+
+);
