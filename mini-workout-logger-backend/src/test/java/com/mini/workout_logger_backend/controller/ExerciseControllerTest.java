9f2f9a4455ffe4874b23dd47e4b42c15c25379f1
Carolina Naccarato <naccaratocarolina@gmail.com>
2025-12-27 13:20:30 -0300

    Making test classes extend AbstractCrudControllerTest

--- mini-workout-logger-backend/src/test/java/com/mini/workout_logger_backend/controller/ExerciseControllerTest.java
+++ mini-workout-logger-backend/src/test/java/com/mini/workout_logger_backend/controller/ExerciseControllerTest.java
@@ -1,131 +1,30 @@
 package com.mini.workout_logger_backend.controller;
 
-import com.mini.workout_logger_backend.AbstractIntegrationTest;
+import com.mini.java_core.AbstractCrudControllerTest;
 import com.mini.workout_logger_backend.dto.ExerciseDTO;
 import com.mini.workout_logger_backend.entity.Exercise;
 import com.mini.workout_logger_backend.mapper.ExerciseMapper;
 import com.mini.workout_logger_backend.repository.ExerciseRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+class ExerciseControllerTest extends AbstractCrudControllerTest<Exercise,
+                                                                ExerciseDTO,
+                                                                ExerciseMapper,
+                                                                ExerciseRepository> {
 
-@AutoConfigureMockMvc
-class ExerciseControllerTest extends AbstractIntegrationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ExerciseRepository exerciseRepository;
-
-    @Autowired
-    private ExerciseMapper exerciseMapper;
-
-    private final ExerciseDTO TEST_EXERCISE_DTO = new ExerciseDTO("Chest Fly");
-
-    @BeforeEach
-    void clean() {
-        exerciseRepository.deleteAll();
+    @Override
+    protected String getBaseUrl() {
+        return "/exercises";
     }
 
-    @Test
-    void _get() throws Exception {
-        // Data.
-        final Exercise savedExercise = exerciseRepository.save(this.exerciseMapper.toEntity(TEST_EXERCISE_DTO));
-
-        // Perform.
-        mockMvc.perform(get("/exercises/{id}", savedExercise.getId())
-                        .queryParam("lang", "pt_BR")
-                        .accept(MediaType.ALL_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data[0].name").value(savedExercise.getName().getValue()));
-    }
-
-    @Test
-    void _list() throws Exception {
-        // Data.
-        List<ExerciseDTO> exercises = List.of(
-                new ExerciseDTO("Deadlift"),
-                new ExerciseDTO("Squat"),
-                new ExerciseDTO("Bench Press")
+    @Override
+    protected List getTestDtos() {
+        return List.of(
+                new ExerciseDTO("Chest Fly"),
+                new ExerciseDTO("Push-Up"),
+                new ExerciseDTO("Squat")
         );
-        exercises.forEach(dto -> exerciseRepository.save(this.exerciseMapper.toEntity(dto)));
-
-        // Perform.
-        mockMvc.perform(get("/exercises")
-                        .queryParam("lang", "pt_BR")
-                        .accept(MediaType.ALL_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.length()").value(exercises.size()));
-    }
-
-    @Test
-    void _create() throws Exception {
-        // Perform.
-        mockMvc.perform(post("/exercises")
-                        .queryParam("lang", "pt_BR")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.ALL_VALUE)
-                        .content(exerciseMapper.toString(TEST_EXERCISE_DTO)))
-                .andDo(print())
-                .andExpect(status().isCreated());
-
-        // Verify.
-        assertThat(exerciseRepository.count()).isEqualTo(1);
-    }
-
-    @Test
-    void _update() throws Exception {
-        // Data.
-        final Exercise savedExercise = exerciseRepository.save(this.exerciseMapper.toEntity(TEST_EXERCISE_DTO));
-
-        // Update.
-        final ExerciseDTO exerciseDto = new ExerciseDTO("Fly");
-
-        // Perform.
-        mockMvc.perform(put("/exercises/{id}", savedExercise.getId())
-                        .queryParam("lang", "pt_BR")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.ALL_VALUE)
-                        .content(exerciseMapper.toString(exerciseDto)))
-                .andDo(print())
-                .andExpect(status().isOk());
-
-        // Verify.
-        final Exercise updatedExercise = exerciseRepository.findById(savedExercise.getId()).orElseThrow();
-        assertThat(updatedExercise.getName().getValue()).isEqualTo(exerciseDto.getName());
-    }
-
-    @Test
-    void _delete() throws Exception {
-        // Data.
-        final Exercise savedExercise = exerciseRepository.save(this.exerciseMapper.toEntity(TEST_EXERCISE_DTO));
-
-        // Perform.
-        mockMvc.perform(delete("/exercises/{id}", savedExercise.getId())
-                        .queryParam("lang", "pt_BR")
-                        .accept(MediaType.ALL_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk());
-
-        // Verify.
-        assertThat(exerciseRepository.existsById(savedExercise.getId())).isFalse();
     }
 
 }
--- mini-workout-logger-backend/src/test/java/com/mini/workout_logger_backend/controller/MuscleControllerTest.java
+++ mini-workout-logger-backend/src/test/java/com/mini/workout_logger_backend/controller/MuscleControllerTest.java
@@ -1,131 +1,30 @@
 package com.mini.workout_logger_backend.controller;
 
-import com.mini.workout_logger_backend.AbstractIntegrationTest;
+import com.mini.java_core.AbstractCrudControllerTest;
 import com.mini.workout_logger_backend.dto.MuscleDTO;
 import com.mini.workout_logger_backend.entity.Muscle;
 import com.mini.workout_logger_backend.mapper.MuscleMapper;
 import com.mini.workout_logger_backend.repository.MuscleRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+class MuscleControllerTest extends AbstractCrudControllerTest<Muscle,
+                                                              MuscleDTO,
+                                                              MuscleMapper,
+                                                              MuscleRepository> {
 
-@AutoConfigureMockMvc
-class MuscleControllerTest extends AbstractIntegrationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private MuscleRepository muscleRepository;
-
-    @Autowired
-    private MuscleMapper muscleMapper;
-
-    private final MuscleDTO TEST_MUSCLE_DTO = new MuscleDTO("Biceps", null);
-
-    @BeforeEach
-    void clean() {
-        muscleRepository.deleteAll();
+    @Override
+    protected String getBaseUrl() {
+        return "/muscles";
     }
 
-    @Test
-    void _get() throws Exception {
-        // Data.
-        final Muscle savedMuscle = muscleRepository.save(this.muscleMapper.toEntity(TEST_MUSCLE_DTO));
-
-        // Perform.
-        mockMvc.perform(get("/muscles/{id}", savedMuscle.getId())
-                        .queryParam("lang", "pt_BR")
-                        .accept(MediaType.ALL_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data[0].name").value(savedMuscle.getName().getValue()));
-    }
-
-    @Test
-    void _list() throws Exception {
-        // Data.
-        List<MuscleDTO> muscles = List.of(
+    @Override
+    protected List getTestDtos() {
+        return List.of(
                 new MuscleDTO("Biceps", null),
                 new MuscleDTO("Triceps", null),
                 new MuscleDTO("Deltoids", null)
         );
-        muscles.forEach(dto -> muscleRepository.save(this.muscleMapper.toEntity(dto)));
-
-        // Perform.
-        mockMvc.perform(get("/muscles")
-                .queryParam("lang", "pt_BR")
-                .accept(MediaType.ALL_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.length()").value(muscles.size()));
-    }
-
-    @Test
-    void _create() throws Exception {
-        // Perform.
-        mockMvc.perform(post("/muscles")
-                .queryParam("lang", "pt_BR")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.ALL_VALUE)
-                .content(muscleMapper.toString(TEST_MUSCLE_DTO)))
-                .andDo(print())
-                .andExpect(status().isCreated());
-
-        // Verify.
-        assertThat(muscleRepository.count()).isEqualTo(1);
-    }
-
-    @Test
-    void _update() throws Exception {
-        // Data.
-        final Muscle savedMuscle = muscleRepository.save(this.muscleMapper.toEntity(TEST_MUSCLE_DTO));
-
-        // Update.
-        final MuscleDTO muscleDto = new MuscleDTO("Triceps", null);
-
-        // Perform.
-        mockMvc.perform(put("/muscles/{id}", savedMuscle.getId())
-                .queryParam("lang", "pt_BR")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.ALL_VALUE)
-                .content(muscleMapper.toString(muscleDto)))
-                .andDo(print())
-                .andExpect(status().isOk());
-
-        // Verify.
-        final Muscle updatedMuscle = muscleRepository.findById(savedMuscle.getId()).orElseThrow();
-        assertThat(updatedMuscle.getName().getValue()).isEqualTo(muscleDto.getName());
-    }
-
-    @Test
-    void _delete() throws Exception {
-        // Data.
-        final Muscle savedMuscle = muscleRepository.save(this.muscleMapper.toEntity(TEST_MUSCLE_DTO));
-
-        // Perform.
-        mockMvc.perform(delete("/muscles/{id}", savedMuscle.getId())
-                .queryParam("lang", "pt_BR")
-                .accept(MediaType.ALL_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk());
-
-        // Verify.
-        assertThat(muscleRepository.existsById(savedMuscle.getId())).isFalse();
     }
 
 }
