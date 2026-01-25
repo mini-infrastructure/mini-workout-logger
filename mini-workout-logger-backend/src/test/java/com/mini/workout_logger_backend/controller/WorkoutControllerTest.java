836c4f3119d736057f8f9c07740ca3f582dcb75f
Carolina Naccarato <naccaratocarolina@gmail.com>
2026-01-24 22:11:17 -0300

    Workout Exercise CRUD test

--- mini-workout-logger-backend/src/test/java/com/mini/workout_logger_backend/controller/WorkoutControllerTest.java
+++ mini-workout-logger-backend/src/test/java/com/mini/workout_logger_backend/controller/WorkoutControllerTest.java
@@ -1,22 +1,34 @@
 package com.mini.workout_logger_backend.controller;
 
+import com.jayway.jsonpath.JsonPath;
 import com.mini.java_core.AbstractCrudControllerTest;
 import com.mini.workout_logger_backend.dto.*;
 import com.mini.workout_logger_backend.entity.Exercise;
 import com.mini.workout_logger_backend.entity.Muscle;
 import com.mini.workout_logger_backend.entity.Workout;
+import com.mini.workout_logger_backend.entity.WorkoutExercise;
 import com.mini.workout_logger_backend.enums.*;
-import com.mini.workout_logger_backend.mapper.ExerciseMapper;
-import com.mini.workout_logger_backend.mapper.MuscleMapper;
-import com.mini.workout_logger_backend.mapper.WorkoutMapper;
-import com.mini.workout_logger_backend.repository.ExerciseRepository;
-import com.mini.workout_logger_backend.repository.MuscleRepository;
-import com.mini.workout_logger_backend.repository.WorkoutRepository;
+import com.mini.workout_logger_backend.mapper.*;
+import com.mini.workout_logger_backend.repository.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class WorkoutControllerTest extends AbstractCrudControllerTest<Workout,
                                                                       WorkoutReadDTO,
@@ -29,13 +41,25 @@ public class WorkoutControllerTest extends AbstractCrudControllerTest<Workout,
     @Autowired
     private MuscleRepository muscleRepository;
 
-    private List<Muscle> savedMuscles;
-
     @Autowired
     private ExerciseMapper exerciseMapper;
 
     @Autowired
     private ExerciseRepository exerciseRepository;
+
+    @Autowired
+    private WorkoutExerciseRepository workoutExerciseRepository;
+
+    @Autowired
+    private WorkoutExerciseMapper workoutExerciseMapper;
+
+    @Autowired
+    private SetMapper setMapper;
+
+    @Autowired
+    private SetRepository setRepository;
+
+    private List<Muscle> savedMuscles;
 
     private List<Exercise> savedExercises;
 
@@ -128,8 +152,130 @@ public class WorkoutControllerTest extends AbstractCrudControllerTest<Workout,
                                         Set.of(savedMuscles.get(2).getId())
                                 )
                         )
+                ),
+                exerciseRepository.save(
+                        exerciseMapper.toEntity(
+                                new ExerciseWriteDTO(
+                                        "Deadlift",
+                                        ExerciseCategory.STRENGTH,
+                                        ExerciseDifficulty.INTERMEDIATE,
+                                        Set.of(savedMuscles.get(1).getId(), savedMuscles.get(2).getId())
+                                )
+                        )
                 )
         );
+    }
+
+    /**
+     * Assert that WorkoutExercises and Sets are correctly associated after creating a workout.
+     * @param dto The created WorkoutReadDTO
+     */
+    @Override
+    protected void afterCreate(WorkoutReadDTO dto) {
+        if (dto.getName().equals("Upper Body Workout")) {
+            List<WorkoutExercise> actualWorkoutExercises = workoutExerciseRepository.findAll()
+                    .stream()
+                    .filter(we -> we.getWorkout().getId().equals(dto.getId()))
+                    .toList();
+            List<com.mini.workout_logger_backend.entity.Set> actualSets = setRepository.findAll()
+                    .stream()
+                    .filter(s -> s.getWorkoutExercise().getWorkout().getId().equals(dto.getId()))
+                    .toList();
+            Map<Long, List<com.mini.workout_logger_backend.entity.Set>> setsByWorkoutExerciseId =
+                    actualSets.stream()
+                            .collect(Collectors.groupingBy(
+                                    s -> s.getWorkoutExercise().getId()
+                            ));
+
+            assertThat(actualWorkoutExercises).hasSize(2);
+            assertThat(actualWorkoutExercises.get(0).getExercise().getName().getValue())
+                    .isEqualTo("Push-Up");
+            assertThat(actualWorkoutExercises.get(1).getExercise().getName().getValue())
+                    .isEqualTo("Squat");
+
+            WorkoutExercise we1 = actualWorkoutExercises.get(0);
+            WorkoutExercise we2 = actualWorkoutExercises.get(1);
+
+            assertThat(actualSets).hasSize(6);
+            assertThat(setsByWorkoutExerciseId.get(we1.getId())).hasSize(3);
+            assertThat(we1.getExercise().getName().getValue()).isEqualTo("Push-Up");
+            assertThat(setsByWorkoutExerciseId.get(we2.getId())).hasSize(3);
+            assertThat(we2.getExercise().getName().getValue()).isEqualTo("Squat");
+        }
+    }
+
+    @Test
+    void _listExercisesAndSets() throws Exception {
+        // Create workout.
+        Workout workout = repository().save(mapper().toEntity(getWriteDtos().getFirst()));
+
+        // List exercises.
+        MvcResult exercisesResult = mockMvc.perform(
+                        get(getBaseUrl() + "/{id}/exercises", workout.getId())
+                                .queryParam("lang", "pt_BR")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.length()").value(2))
+                .andExpect(jsonPath("$.data[0].exercise.name").value("Push-Up"))
+                .andReturn();
+
+        // Extract workoutExerciseId from response.
+        String response = exercisesResult.getResponse().getContentAsString();
+        Long workoutExerciseId = JsonPath
+                .parse(response)
+                .read("$.data[0].id", Long.class);
+
+        // List sets.
+        mockMvc.perform(get(getBaseUrl() + "/{id}/exercises/{exerciseId}/sets",
+                                workout.getId(),
+                                workoutExerciseId)
+                                .queryParam("lang", "pt_BR")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.length()").value(3));
+    }
+
+    @Test
+    void _addExercise() throws Exception {
+        // Create workout.
+        Workout workout = repository().save(mapper().toEntity(getWriteDtos().getFirst()));
+        int oldExerciseCount = workout.getWorkoutExercises().size();
+
+        // Create new exercise.
+        Exercise exercise = exerciseRepository.save(
+                exerciseMapper.toEntity(
+                        new ExerciseWriteDTO(
+                                "Chest-Fly",
+                                ExerciseCategory.STRENGTH,
+                                ExerciseDifficulty.BEGINNER,
+                                Set.of(savedMuscles.getFirst().getId())
+                        )
+                )
+        );
+
+        // Perform.
+        WorkoutExerciseWriteDTO we = new WorkoutExerciseWriteDTO(
+                exercise.getId(),
+                List.of(
+                        new SetWriteDTO(SetCategory.NORMAL, SetType.REPS_X_WEIGHT, 8, 70.0, null),
+                        new SetWriteDTO(SetCategory.NORMAL, SetType.REPS_X_WEIGHT, 8, 75.0, null)
+                ),
+                ExerciseEquipment.BARBELL,
+                60
+        );
+
+        MvcResult exercisesResult = mockMvc.perform(MockMvcRequestBuilders.put(getBaseUrl() + "/{id}/exercises", workout.getId())
+                        .queryParam("lang", "pt_BR")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.ALL_VALUE)
+                        .content(workoutExerciseMapper.toString(we)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.length()").value(oldExerciseCount + 1))
+                .andExpect(jsonPath("$.data[?(@.exercise.name == 'Chest-Fly')]").exists())
+                .andReturn();
+
     }
 
 }
